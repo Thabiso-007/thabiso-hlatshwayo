910e3b1a5a62e8d8b47318f3f1e2f29c630355c9
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-16 21:28:06 +0200

    Added

---
+++ utils/data.js
@@ -0,0 +1,179 @@
+import html from '../public/assets/skills/html.png'
+import css from '../public/assets/skills/css.png'
+import js from '../public/assets/skills/javascript.png'
+import ts from '../public/assets/skills/typescript.png'
+import git from '../public/assets/skills/git.png'
+import github from '../public/assets/skills/github.png'
+import react from '../public/assets/skills/react.png'
+import vue from '../public/assets/skills/vue.png'
+import python from '../public/assets/skills/python.png'
+import flask from '../public/assets/skills/flask.png'
+import java from '../public/assets/skills/java.png'
+import spring from '../public/assets/skills/spring.png'
+import redux from '../public/assets/skills/redux.png'
+import mysql from '../public/assets/skills/mysql.png'
+import sqlalchemy from '../public/assets/skills/sqlAlchemy.png'
+import mongo from '../public/assets/skills/mongodb.png'
+import tailwind from '../public/assets/skills/tailwind-css.png'
+import bootstrap from '../public/assets/skills/bootstrap.png'
+import node from '../public/assets/skills/node.png'
+import express from '../public/assets/skills/express.png'
+import next from '../public/assets/skills/nextjs.png'
+import graphql from '../public/assets/skills/graphql.png'
+
+export const data = [
+    {
+        id: 1,
+        name: "HTML",
+        image: html,
+        weight: 64,
+        height: 64 
+    },
+    {
+        id: 2,
+        name: "CSS",
+        image: css,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 3,
+        name: "JavaScrip",
+        image: js,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 4,
+        name: "TypeScript",
+        image: ts,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 5,
+        name: "Git",
+        image: git,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 6,
+        name: "Github",
+        image: github,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 7,
+        name: "React",
+        image: react,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 8,
+        name: "Vue",
+        image: vue,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 9,
+        name: "Python",
+        image: python,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 10,
+        name: "Flask",
+        image: flask,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 11,
+        name: "Java",
+        image: java,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 12,
+        name: "Spring Boot",
+        image: spring,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 13,
+        name: "Redux",
+        image: redux,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 14,
+        name: "sqlAlchemy",
+        image: sqlalchemy,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 15,
+        name: "MySQL",
+        image: mysql,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 16,
+        name: "Mongodb",
+        image: mongo,
+        weight: 64,
+        height: "44"
+    },
+    {
+        id: 17,
+        name: "tailwind CSS",
+        image: tailwind,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 18,
+        name: "Bootstrap",
+        image: bootstrap,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 19,
+        name: "Node.js",
+        image: node,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 20,
+        name: "Express.js",
+        image: express,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 21,
+        name: "GraphQL",
+        image: graphql,
+        weight: 64,
+        height: 64
+    },
+    {
+        id: 22,
+        name: "Next.js",
+        image: next,
+        weight: 64,
+        height: 64
+    }
+]
