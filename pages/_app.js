374b9762e065bcaa36e4a60f3d22a562df4e0c46
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-16 21:37:21 +0200

    Modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 
 import '../styles/globals.css'
-import NavBar from '../components/nav-bar/NavBar'
 import Footer from '../components/footer/Footer';
+import NavBar from '../components/nav-bar/NavBar';
 
 function MyApp({ Component, pageProps }) {
   return (
